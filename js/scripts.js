a0a695c53d422a8f1e3f298871ddc7b68693d6cd
CorinnaK <corinna.kelly@outlook.com>
2020-07-21 20:19:04 -0600

    Refactored the validating section for inputs and moved to a function, combing textarea and inputs. Renamed variables for readability.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 
+        // Array of words that I want filtered
+        const unProfWords = ["feldercarb", "frack", "skinjob", "vulgacarb"];
+
+        let errorMessages = [];
+
 // Citation
 // https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
 // Loops over all the elements with the contact class
-
 document.querySelectorAll(".contact").forEach(link => {
     link.addEventListener("click", () => {
         
@@ -10,97 +14,62 @@ document.querySelectorAll(".contact").forEach(link => {
         document.querySelector(".form").classList.remove("hiddenForm");
         document.querySelector(".form").classList.add("displayForm");
 
-        // Array of words that I want filtered
-        const unProfWords = ["feldercarb", "frack", "skinjob", "vulgacarb"];
-        const inputs = document.querySelectorAll("input");
-        const textArea = document.querySelector("textarea");
-        let errors = [];
+        // Toggel form from displayed to hidden when the user clicks the cancel button
         document.querySelector("#cancel").addEventListener("click", () =>{
             event.preventDefault();
-            // Toggle class to hide the form if the user clicks cancel
             document.querySelector(".form").classList.add("hiddenForm");
             document.querySelector(".form").classList.remove("displayForm");
         })
 
+        // Validate form information when the user clicks submit
         document.querySelector("#submit").addEventListener("click", () =>{
             event.preventDefault();
-            const listErrorClass = document.querySelector("form ul");
-            
-            // Reference: https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
-            // Changed ideas from looping over childern to looping over the list items and removing the actual li
-            let errorsPresent = listErrorClass.querySelectorAll("li");
-            
-            // Remove any errors list items from the form
-            for (listItems of errorsPresent){
-                listItems.remove();
-            }
 
-            // Loop over the input fields
+            // Access the unordered list that displays the errors in the form
+            const errorPanel = document.querySelector(".errPanel");
+
+            // Access any/all of the error list items currently in the unordered list
+            let errorListItems = errorPanel.querySelectorAll("li");
+            
+            // Remove any errors list items currently displayed in form
+            for (eachErrorInList of errorListItems){
+                eachErrorInList.remove();
+            }
+            const inputs = document.querySelectorAll("input");
+            const textArea = document.querySelector("textarea");
             for (input of inputs)
             {
-                // Remove the errors class indicator to start fresh if the user is resubmitting
-                input.classList.remove("errors");
-                // Use Validate email function here if sending form data directly using input as the argument
-                if (input.value.length == 0){
-                    errors.push("Please fill out the " + input.id + " field.");
-                }
-                // Retain the input id to use for adding the error class back to the input id
-                inputId = input;
-                // Grab the value of the input and transform to lower case 
-                input = input.value;
-                input  = input.toLowerCase();
-                // Loop through the list of unproffesional words and check if they are part of the string
-                for (word of unProfWords){
-                    // The index is used to check the location of the starting letter of the word = 0 or above means it is found
-                    if (input.indexOf(word) > -1){
-                        // **ToDo add functionality to highlight unProf word in input** 
-                        // Adds the error to the main error list and adds the class errors to input
-                        errors.push("* " + word + "* is an unprofessional word. Please replace it with something more appropriate.");
-                        inputId.classList.add("errors");
-                    }     
-                }
+                validateForm(input);
             }
-            // Check to make sure that message area has input and that no unprofessional words are used
-            // Must be outside of the input iterator or it will loop and produce multiple errors    
-            if (textArea.value.length == 0){
-                errors.push("Please fill out the message field.");
-            }
-            textArea.classList.remove("errors");
-            for (word of unProfWords){
-                textAreaWords = textArea.value;
-                textAreaWords = textAreaWords.toLowerCase();
-                if (textAreaWords.indexOf(word) > -1){
-                    errors.push("*" + word + "*  is an unprofessional word. Please replace it with something more appropriate");
-                    textarea.classList.add("errors");
-                }     
-            }
+            validateForm(textArea);
+
             // Citation starts
             // https://github.com/TECHCareers-by-Manpower/js-practice
             // Followed the example in the JS Practice Shopping List for adding and removing classes
             // Syntax element.classList.add/remove(class)
             // If no errors are found change change the classList to noErrrors if it wasn't already set and remove the errors class
-            if (errors.length == 0){
-                if (listErrorClass.classList != "noErrors"){
-                    listErrorClass.classList.add("noErrors")
-                    listErrorClass.classList.remove("errors");
+            if (errorMessages.length == 0){
+                if (errorPanel.classList != "noErrors"){
+                    errorPanel.classList.add("noErrors")
+                    errorPanel.classList.remove("errors");
                 }
                 // sumbit inputs to email mailto
                 Redirect();
-            }
+            } 
             else
             {
                 // create errors li and add them to the ul
-                for (error of errors){
+                for (error of errorMessages){
                     let newError = document.createElement("li");
                     newError.innerText = error;
-                    listErrorClass.appendChild(newError);
+                    errorPanel.appendChild(newError);
                     // Toggle class to errors if not already set  to errors
-                    if (listErrorClass.classList != "errors"){
-                        listErrorClass.classList.add("errors");
-                        listErrorClass.classList.remove("noErrors");
+                    if (errorPanel.classList != "errors"){
+                        errorPanel.classList.add("errors");
+                        errorPanel.classList.remove("noErrors");
                     }
                 // Clears the errors array preparing for the next submit click event    
-                errors = [];
+                errorMessages = [];
                 }
             }
             // End Citation
@@ -110,6 +79,7 @@ document.querySelectorAll(".contact").forEach(link => {
 // Ensure user inputs a valid email address into a form. Takes the input as the argument and outputs an error message to the errors array.
 // A valid email for this functions is ***@***.**
 function ValidateEmail (input){
+
     // Validate the email field if sending the form information directly
     // Not neccessary when using mailto
     if (input.type == "email"){
@@ -123,7 +93,7 @@ function ValidateEmail (input){
             || (input.value.indexOf(".") < (input.value.indexOf("@")+3)) 
             || ((input.value.lastIndexOf(".") + 3)  > (input.value.length))
             ) {
-            errors.push("Please enter a valid email address");
+            errorMessages.push("Please enter a valid email address");
         }
         // End Citation
     }
@@ -135,3 +105,47 @@ function Redirect()
     // How to add line breaks into body of mail
     window.location.href = "mailto:husker@galaxyhit.com?&subject=" + document.querySelector("#subject").value + "&body=" + document.querySelector("#message").value + "%0D%0A%0D%0ASent from " + document.querySelector("#name").value;
 }
+
+function validateForm (input)
+{
+
+    input.classList.remove("errors");
+    if (input.value.length == 0){
+        errorMessages.push("Please fill out the " + input.id + " field.");
+    }
+    for (word of unProfWords){
+        inputValue = input.value;
+        inputValue = inputValue.toLowerCase();
+        if (inputValue.indexOf(word) > -1){
+
+            errorMessages.push("*" + word + "*  is an unprofessional word. Please replace it with something more appropriate");
+            
+            // Add the class of errors to the input field to style with red color
+            input.classList.add("errors");
+        }
+    }         
+}                // // Remove the errors class indicator to start fresh if the user is resubmitting
+// input.classList.remove("errors");
+
+// // Use Validate email function here if sending form data directly using input as the argument
+
+// // Grab the value of the input and transform to lower case 
+// inputValue = input.value;
+// inputValue  = inputValue.toLowerCase();
+
+// // Ensure that the user enters a value in each input field
+// if (inputValue.length == 0){
+//     errors.push("Please fill out the " + input.id + " field.");
+// }
+
+
+// // Loop through the list of unproffesional words and check if they are part of the string
+// for (word of unProfWords){
+
+//     // The index is used to check the location of the starting letter of the word = 0 or above means it is found
+//     if (inputValue.indexOf(word) > -1){
+
+//         // **ToDo add functionality to highlight unProf word in input** 
+//         // Adds the error to the main error list and adds the class errors to input
+//         errors.push("*" + word + "* is an unprofessional word. Please replace it with something more appropriate.");
+//         input.classList.add("errors");
